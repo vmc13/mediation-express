7c5dd0aa021d0b819668f98b20a63d07e116b815
Victória Mendes <vick.costa1305@gmail.com>
2025-01-13 19:15:09 -0300

    core: adicionando arquivos firebase ao gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_options.dart';
 import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
